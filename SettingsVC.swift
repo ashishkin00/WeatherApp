9bc594696f0b56c4ffb38134729ad089bf5f81a3
Andrey Shishkin <a.shishkin@dunice.net>
2022-11-21 10:11:44 +0300

    Update

--- SettingsVC.swift
+++ SettingsVC.swift
@@ -20,10 +20,10 @@ class SettingsVC: UIViewController {
     
     func makeConstraints() {
         NSLayoutConstraint.activate([
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
 }
--- SettingsVCDelegate.swift
+++ SettingsVCDelegate.swift
@@ -1,13 +1,30 @@
 import UIKit
 
+enum Months: String, CaseIterable {
+    case january = "JAN"
+    case february = "FEB"
+    case march = "MAR"
+    case april = "APR"
+    case may = "MAY"
+    case june = "JUN"
+    case july = "JUL"
+    case august = "AUG"
+    case september = "SEP"
+    case october = "OCT"
+    case november = "NOV"
+    case december = "DEC"
+}
+
 extension SettingsVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let spacing = CGFloat(10)
-        return UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        let spacing = CGFloat(0)
+        return UIEdgeInsets(top: spacing, left: 10, bottom: 15, right: 10)
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! CollectionViewCell
-        cell.label.text = "\(indexPath)"
+        let months = Months.allCases
+        print(indexPath.item, indexPath.section)
+        cell.label.text = months[indexPath.item + (indexPath.section * 6)].rawValue
         cell.backgroundColor = .red
         return cell
     }
@@ -18,5 +35,14 @@ extension SettingsVC: UICollectionViewDelegate, UICollectionViewDataSource, UICo
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewSize = collectionView.frame.size.width - 50
+        return CGSize(width: collectionViewSize / 7, height: collectionViewSize / 7)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(Months.allCases[indexPath.item + (indexPath.section * 6)].rawValue)
     }
 }
